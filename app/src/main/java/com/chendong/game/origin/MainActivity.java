3d73ed0a8dcaa29be401ecf67416bf02a9ea02d4
陈东 <chendong@renwey.com>
2016-11-01 16:35:26 +0800

    新功能

--- app/src/main/java/com/chendong/game/origin/MainActivity.java
+++ app/src/main/java/com/chendong/game/origin/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //测试分支，修改了一个bug
-        //然后我加上我的新功能一起发布出去
-        //test
+
     }
 }
